d40a551730c3965618f1ddfab9a7c288954576af
Hashbang173 <hashbang173@gmail.com>
2018-12-21 14:24:27 -0500

    clark: Omni-fy
    Change-Id: Iffef1cf3c7823da4a12e1ef4ce5836ffe515ecab

--- omni_clark.mk
+++ omni_clark.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_clark
+PRODUCT_NAME := omni_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
